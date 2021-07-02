cbdafef3f7cdd1e265602396d2b9e50fddb74cac
Anuj Kumar <anujkumar29012000@gmail.com>
2021-07-02 12:13:55 +0530

    More Web UI Improvements: Allowing users to resummarize new video link.
    - Format of API Request:
    https://ytsum.herokuapp.com/summarize/?id=<video-id>&percent=<percent>&choice=<summary-choice>
    
    - Web Version of Summarizer:
    https://ytsum.herokuapp.com/web/

--- static/web.js
+++ static/web.js
@@ -5,21 +5,20 @@ const percent_dropdown = document.getElementById('percent-dropdown');
 const choice_dropdown = document.getElementById('summary-dropdown');
 
 const youtube_div = document.getElementById("youtube");
-let text_out_main_div = document.getElementById("text_out_main");
+const text_out_main_div = document.getElementById("text_out_main");
+const re_summarize_element = document.getElementById('re_summarize');
 
 // Button Click Listener for InitializeSummary() Function
 summarize_button.addEventListener("click", initializeSummary);
 
 // Making Summarize button disabled on start
 summarize_button.disabled = true;
+// Making Re-summarize element hidden at start: till summary is visible
+re_summarize_element.style.display = "none";
 
 // Adding EventListeners on both dropdowns to enable button when both dropdowns have selected a valid choice.
-percent_dropdown.addEventListener("click", buttonUpdate);
-choice_dropdown.addEventListener("click", buttonUpdate);
 percent_dropdown.addEventListener("change", buttonUpdate);
 choice_dropdown.addEventListener("change", buttonUpdate);
-percent_dropdown.addEventListener("keyup", buttonUpdate);
-choice_dropdown.addEventListener("keyup", buttonUpdate);
 url_input.addEventListener("input", buttonUpdate);
 
 function buttonUpdate() {
@@ -40,11 +39,11 @@ function initializeSummary() {
         let choice_index = choice_dropdown.selectedIndex;
 
         // Storing URL of the URL present in the text box
-        var url = url_input.value;
+        const url = url_input.value;
 
-        var video_id = parse_youtube_video_id(url);
-        var percent = percent_value.split("%")[0];
-        var choice = parse_choice(choice_index);
+        const video_id = parse_youtube_video_id(url);
+        const percent = percent_value.split("%")[0];
+        const choice = parse_choice(choice_index);
 
         if (video_id) {
             // Making Selection Div invisible, and text output as visible.
@@ -62,16 +61,24 @@ function initializeSummary() {
                 process_element.innerHTML = result.message;
                 if (result.success) {
                     // If result was successfully received. Then, parse the result_response JSON
-                    var response_json = (result.response[0]);
+                    const response_json = (result.response[0]);
+
+                    // Use the values present in JSON for displaying summary.
                     text_out_content_element.innerHTML = "Processed Summary: " + response_json.processed_summary
                         + "<p>Characters in Original Transcript: " + response_json.length_original
                         + "<br>Characters in Summary: " + response_json.length_summary + "</p><br>";
+
+                    // Text Beautification: Aligning Text to be justified
                     text_out_content_element.style.textAlign = "justify";
                     text_out_content_element.style.textJustify = "inter-word";
+                    // Enabling re-summarize element
+                    re_summarize_element.style.display = "block";
                 } else {
                     // We failed: Reason is already pushed to UI in process_element (response.result_message has reason)
                     text_out_content_element.innerHTML = "We failed due to above reason.";
                     text_out_content_element.style.textAlign = "center";
+                    // Disabling re-summarize element
+                    re_summarize_element.style.display = "none";
                 }
             }).catch(error => {
                 // Network issue occurred during fetch probably. Logging and sending result backs.
@@ -82,7 +89,8 @@ function initializeSummary() {
                 text_out_content_element.style.textAlign = "center";
             })
         } else {
-            alert("Your Video URL is invalid.");
+            // Alerting user that they entered wrong URL
+            alert("Your YouTube video URL is invalid. Please retry.");
             url_input.value = "";
         }
     }
@@ -90,8 +98,8 @@ function initializeSummary() {
 
 function parse_youtube_video_id(url) {
     // This function returns video id if it is a valid youtube video. Else it returns false
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    const match = url.match(regExp);
     return (match && match[7].length === 11) ? match[7] : false;
 }
 
